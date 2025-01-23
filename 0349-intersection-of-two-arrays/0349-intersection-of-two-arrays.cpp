72b97e636db23772c4cfe6f912990ffcf07f48dc
Nguyen Minh <161208761+nminh2209@users.noreply.github.com>
2025-01-23 21:53:22 +0700

    Time: 0 ms (100%), Space: 14.6 MB (15.67%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1(nums1.begin(), nums1.end()); 
+        unordered_set<int> resultSet;
+
+       
+        for (int num : nums2) {
+            if (set1.count(num)) { 
+                resultSet.insert(num);
+            }
+        }
+
+        
+        return vector<int>(resultSet.begin(), resultSet.end());
+    }
+};
